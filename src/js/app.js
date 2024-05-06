fb6fe6b974eaba5fe6deac058fa91311377e873a
Cristian2499 <162387827+Cristian2499@users.noreply.github.com>
2024-05-06 19:03:38 +0000

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -45,16 +45,16 @@ function render(variables = {}) {
               ? "position-left"
               : "position-right"
           }">
-            <li><a href="https://twitter.com/4geeksacademy${
+            <li><a href="https://twitter.com/${
               variables.twitter == null ? "twitter" : variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy${
+            <li><a href="https://github.com/${
               variables.github == null ? "gihub" : variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy${
+            <li><a href="https://www.linkedin.com/${
               variables.linkedin == null ? "linkedin" : variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy${
+            <li><a href="https://www.instagram.com/${
               variables.instagram == null ? "instagram" : variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
